1f116818021b7c4c3f44de760686adbc24f77314
Emberizaf <1522236760@qq.com>
2024-09-28 23:57:48 +0800

    Site updated: 2024-09-28 23:57:48

--- scripts/injects.js
+++ scripts/injects.js
@@ -1,9 +1,6 @@
 // 页面加载 css
-hexo.extend.injector.register("body_end",'<script type="text/css" src="/css/loader.css"></script>');
+hexo.extend.injector.register("head_start",'<script type="text/css" src="/css/loader.css"></script>');
 
 //页面加载 html
-hexo.extend.injector.register("body_end",'<script src="/html/loader.html"></script>');
-
-// 页面加载 js
-hexo.extend.injector.register("body_end",'<script type="text/javascript" src="/js/loader.js"></script>');
+hexo.extend.injector.register("head_start",'<script src="/html/loader.html"></script>');
 
