f46a692d6aaa1c312f4c4651bab81666d549e6fd
Emberizaf <1522236760@qq.com>
2024-09-29 21:12:09 +0800

    Site updated: 2024-09-29 21:12:07

--- js/loader.js
+++ js/loader.js
@@ -1,9 +1,3 @@
-// $(function(){
-//     $("#loader").fadeOut(600);
-//  });
-
- $(function() {
-    setTimeout(function() {
-        $("#loader").fadeOut(540);
-    }, 2000); // 2秒延迟
-});
+$(function(){
+    $("#Loadanimation").fadeOut(500);
+ });
--- scripts/injects.js
+++ scripts/injects.js
@@ -1,6 +1,17 @@
-// 页面加载 css
-hexo.extend.injector.register("head_start",'<script type="text/css" src="/css/loader.css"></script>');
+// hexo.extend.injector.register("body_end", '<div class="loader"></div>'); // 注入 loader HTML
+// hexo.extend.injector.register("body_end", '<script src="/js/loader.js"></script>'); // 引入 JavaScript
 
-//页面加载 html
-hexo.extend.injector.register("head_start",'<script src="/html/loader.html"></script>');
-
+hexo.extend.filter.register('theme_inject', function(injects) {
+    injects.bodyBegin.raw('default', `
+        <div id="Loadanimation" style="z-index:999999;">
+            <div id="Loadanimation-center">
+                <div id="Loadanimation-center-absolute">
+                    <div class="xccx_object" id="xccx_four"></div>
+                    <div class="xccx_object" id="xccx_three"></div>
+                    <div class="xccx_object" id="xccx_two"></div>
+                    <div class="xccx_object" id="xccx_one"></div>
+                </div>
+            </div>
+        </div>
+`);
+  });
